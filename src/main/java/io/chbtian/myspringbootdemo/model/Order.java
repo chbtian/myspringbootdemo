83667d0dcfaa5432d9dfb14ea69a7adb8eab6cbd
tcb <chbtian@163.com>
2019-04-04 12:38:22 +0800

    commit test

---
+++ src/main/java/io/chbtian/myspringbootdemo/model/Order.java
@@ -0,0 +1,52 @@
+package io.chbtian.myspringbootdemo.model;
+
+import java.text.DecimalFormat;
+
+public class Order {
+    private Integer id;
+    private String name;
+    private Double price;
+    private String desc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
